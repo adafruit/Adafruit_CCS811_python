b84967f6c7649770c6b492d854cdc60cb64e178b
Brennen Bearnes <bbearnes@gmail.com>
2018-08-21 11:36:35 -0600

    bump version for deprecated lib, add readme on pypi

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ except ImportError:
 
 from setuptools import setup, find_packages
 
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 classifiers = ['Development Status :: 4 - Beta',
                'Operating System :: POSIX :: Linux',
                'License :: OSI Approved :: MIT License',
@@ -18,7 +27,7 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: System :: Hardware']
 
 setup(name              = 'Adafruit_CCS811',
-      version           = '0.1',
+      version           = '0.2',
       author            = 'Dean Miller',
       author_email      = 'dean@adafruit.com',
       description       = 'Python library to use the CCS811 air quality sensor with raspberry pi or other linux boards.',
@@ -27,4 +36,7 @@ setup(name              = 'Adafruit_CCS811',
       url               = 'https://github.com/adafruit/Adafruit_CCS811_python/',
       dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
       install_requires  = ['Adafruit-GPIO>=0.6.5', 'Adafruit-bitfield>=1.5'],
-      packages          = find_packages())
+      packages          = find_packages(),
+
+      long_description = long_description,
+      long_description_content_type = 'text/markdown')
